f7a3a0bc43abd63330eaff47832a49828c75bf85
VON EUW Tom <thomas.von.euw@hexagon.com>
2022-12-03 17:00:39 +0100

    Day03

---
+++ AdventOfCode2022.Tests/Day03Tests.cs
@@ -0,0 +1,35 @@
+using AdventOfCode2022.Solutions;
+using NUnit.Framework;
+using System;
+
+namespace AdventOfCode2022.Tests
+{
+    public class Day03Tests
+    {
+        [Test]
+        public void SolvePart1_SmallExample_ReturnsCorrectValue()
+        {
+            var day = new Day03(@"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw");
+
+            Assert.AreEqual(157, Convert.ToInt32(day.Part1));
+        }
+
+        [Test]
+        public void SolvePart2_SmallExample_ReturnsCorrectValue()
+        {
+            var day = new Day03(@"vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw");
+
+            Assert.AreEqual(70, Convert.ToInt32(day.Part2));
+        }
+    }
+}
---
+++ AdventOfCode2022/Solutions/Day03.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2022.Solutions
+{
+    public class Day03 : DayBase
+    {
+        private readonly List<string> _rucksacks;
+
+        public Day03(string input = null) : base(input)
+        {
+            _rucksacks = Input.ToList();
+        }
+
+        protected override object SolvePart1()
+        {
+            int prioritySum = 0;
+            foreach (var rucksack in _rucksacks)
+            {
+                var compartmentSize = rucksack.Length / 2;
+                var firstComp = rucksack.Take(compartmentSize);
+                var secondComp = rucksack.Skip(compartmentSize).Take(compartmentSize);
+
+                var duplicateItem = firstComp.Intersect(secondComp).Single();
+                prioritySum += CalculatePriority(duplicateItem);
+            }
+
+            return prioritySum;
+        }
+
+        protected override object SolvePart2()
+        {
+            int prioritySum = 0;
+            foreach (var groupOfThree in _rucksacks.Chunk(3))
+            {
+                var badgeItem = groupOfThree[0].Intersect(groupOfThree[1]).Intersect(groupOfThree[2]).Single();
+                prioritySum += CalculatePriority(badgeItem);
+            }
+
+            return prioritySum;
+        }
+
+        private static int CalculatePriority(char item)
+        {
+            return char.IsUpper(item) ? item - 38 : item - 96;
+        }
+    }
+}
