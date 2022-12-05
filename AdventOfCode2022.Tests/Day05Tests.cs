61b994d5192b34f9acbd584f12effd06de3d557e
VON EUW Tom <thomas.von.euw@hexagon.com>
2022-12-05 20:15:59 +0100

    Day05

--- AdventOfCode2022.Tests/Day05Tests.cs
+++ AdventOfCode2022.Tests/Day05Tests.cs
@@ -9,17 +9,33 @@ namespace AdventOfCode2022.Tests
         [Test]
         public void SolvePart1_SmallExample_ReturnsCorrectValue()
         {
-            var day = new Day05(@"");
+            var day = new Day05(@"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
 
-            Assert.AreEqual(0, Convert.ToInt32(day.Part1));
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2");
+
+            Assert.AreEqual("CMZ", day.Part1);
         }
 
         [Test]
         public void SolvePart2_SmallExample_ReturnsCorrectValue()
         {
-            var day = new Day05(@"");
+            var day = new Day05(@"    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
 
-            Assert.AreEqual(0, Convert.ToInt32(day.Part2));
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2");
+
+            Assert.AreEqual("MCD", day.Part2);
         }
     }
 }
--- AdventOfCode2022/Solutions/Day05.cs
+++ AdventOfCode2022/Solutions/Day05.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace AdventOfCode2022.Solutions
 {
     public class Day05 : DayBase
     {
-        private readonly List<(string firstAssign, string secondAssign)> _pairs;
+        private readonly Dictionary<int, Stack<char>> _stacks = new Dictionary<int, Stack<char>>();
 
         public Day05(string input = null) : base(input)
         {
@@ -15,15 +16,70 @@ namespace AdventOfCode2022.Solutions
 
         protected override object SolvePart1()
         {
-
-            return 0;
+            return MoveStacks(InitializeStacksAndGetRearrangementProc(), false);
         }
-
-
 
         protected override object SolvePart2()
         {
-            return 0;
+            return MoveStacks(InitializeStacksAndGetRearrangementProc(), true);
+        }
+
+        private string MoveStacks(IEnumerable<string> moves, bool canMoveMultipleStacks)
+        {
+            foreach (var move in moves)
+            {
+                var splitted = move.Split("from");
+                int elementsToMoveCount = Convert.ToInt32(splitted[0].Replace("move", ""));
+                var moveFromTo = splitted[1].Split("to").Select(int.Parse).ToList();
+
+                var pickedCrates = new List<char>();
+                for (int i = 0; i < elementsToMoveCount; i++)
+                {
+                    pickedCrates.Add(_stacks[moveFromTo[0]].Pop());
+                }
+
+                if (canMoveMultipleStacks)
+                {
+                    pickedCrates.Reverse();
+                }
+
+                foreach (var crate in pickedCrates)
+                {
+                    _stacks[moveFromTo[1]].Push(crate);
+                }
+            }
+
+            var moveOrder = new StringBuilder();
+            foreach (var stack in _stacks)
+            {
+                moveOrder.Append(stack.Value.Pop());
+            }
+
+            return moveOrder.ToString();
+        }
+
+        private IEnumerable<string> InitializeStacksAndGetRearrangementProc()
+        {
+            var stackDefinition = Input.Where(r => (r.StartsWith(" ") && r.Trim().Length != 0) || r.StartsWith("[")).ToList();
+            var rearrangementProc = Input.Where(r => r.StartsWith("move"));
+
+            for (int i = stackDefinition.Count - 2; i >= 0; i--)
+            {
+                var stackRow = stackDefinition[i];
+                var stackId = 0;
+                for (int j = 1; j < stackRow.Length; j += 4)
+                {
+                    stackId++;
+                    var crate = stackRow[j];
+                    if (!char.IsWhiteSpace(crate) &&
+                        !_stacks.TryAdd(stackId, new Stack<char>(new List<char> { crate })))
+                    {
+                        _stacks[stackId].Push(crate);
+                    }
+                }
+            }
+
+            return rearrangementProc;
         }
     }
 }
